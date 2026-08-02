d259e648865ff2858a708b4bd1757fee7dfa9825
sai2001-charanteja <saicharantejardss3661@gmail.com>
2026-08-02 00:28:12 -0700

    Variable Probality modified logic

--- Constraints/variable_probability.sv
+++ Constraints/variable_probability.sv
@@ -4,30 +4,23 @@ Write uvm sv constraint for 8-bit variable where next value parity depends on pr
 (2) If previous value was even, next value is even with 25% probability. 
 Maintain state between randomizations.
 */
-
-
 module tb;
   class temp;
   	  rand bit [7:0] val;
-         bit [7:0] prev_val;
-
-    function new();
-        prev_val = 8'h00;      // initial previous value
-    endfunction
+      bit parity;
 
     constraint parity_c {
 
-        if ($countones(prev_val)%2)
-            ($countones(val)%2) dist {0 := 75,
+        if (parity)
+            (^val) dist {0 := 75,
                                       1 := 25};
-
         else
-            ($countones(val)%2) dist {0 := 25,
+            (^val) dist {0 := 25,
                                       1 := 75};
     }
 
     function void post_randomize();
-        prev_val = val;
+        parity = ^val;
       $display("%0b - %0d",val,^val);
     endfunction
    
